317be41b4694d2d85705dce5fcd05ab9530e5822
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-10-05 17:05:57 +0300

    Convert HomeScreen to StatefulWidget, add bottom navigation bar with 5 tabs

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:islami/tabs/hadeth_tab.dart';
+import 'package:islami/tabs/quran_tab.dart';
+import 'package:islami/tabs/radio_tab.dart';
+import 'package:islami/tabs/sebha_tab.dart';
+import 'package:islami/tabs/settings_tab.dart';
+import 'package:islami/utils/app_colors.dart';
 
 import 'utils/app_images.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  List<Widget> tabs = [
+    QuranTab(),
+    HadethTab(),
+    SebhaTab(),
+    RadioTab(),
+    SettingsTab(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +34,49 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Islami',
-          ),
-        ),
+        appBar: AppBar(title: const Text('Islami')),
+        body: tabs[_currentIndex],
+        bottomNavigationBar: customBottomNavigationBar(),
       ),
+    );
+  }
+
+  Widget customBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      selectedItemColor: AppColors.accentColor,
+      onTap: (value) {
+        setState(() {
+          _currentIndex = value;
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(
+          icon: ImageIcon(AssetImage(AppImages.quranIcon)),
+          label: 'Quran',
+          backgroundColor: AppColors.primaryColor,
+        ),
+        BottomNavigationBarItem(
+          icon: ImageIcon(AssetImage(AppImages.hadethIcon)),
+          label: 'Hadeth',
+          backgroundColor: AppColors.primaryColor,
+        ),
+        BottomNavigationBarItem(
+          icon: ImageIcon(AssetImage(AppImages.sebhaIcon)),
+          label: 'Sebha',
+          backgroundColor: AppColors.primaryColor,
+        ),
+        BottomNavigationBarItem(
+          icon: ImageIcon(AssetImage(AppImages.radioIcon)),
+          label: 'Radio',
+          backgroundColor: AppColors.primaryColor,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings_rounded),
+          label: "Settings",
+          backgroundColor: AppColors.primaryColor,
+        ),
+      ],
     );
   }
 }
--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -1,4 +1,9 @@
 class AppImages {
   static const String baseUrl = 'assets/images/';
   static const String background = '${baseUrl}default_bg.png';
+
+  static const String quranIcon = '${baseUrl}icon_quran.png';
+  static const String radioIcon = '${baseUrl}icon_radio.png';
+  static const String sebhaIcon = '${baseUrl}icon_sebha.png';
+  static const String hadethIcon = '${baseUrl}icon_hadeth.png';
 }
