9536753ba764505c78c1b03703da1dd9f7a9bb9d
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-10-05 16:06:39 +0300

    Replaced AppBar with background image and transparent Scaffold and created generic app colors and images classes

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'utils/app_images.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Islami App'),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppImages.background),
+        ),
+      ),
+      child: const Scaffold(
+        backgroundColor: Colors.transparent,
       ),
     );
   }
---
+++ lib/utils/app_images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  static const String baseUrl = 'assets/images/';
+  static const String background = '${baseUrl}default_bg.png';
+}
